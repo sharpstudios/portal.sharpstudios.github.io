3f67bc7dda61efedbb4e59d1d11ab17cc3d5e800
Noger <noah@noger.ml>
2018-11-29 22:02:06 +0000

    Update 0.2.1.2
    Think I found the issue with the sign in, it is a f**king syntax error. I prefer Python from JQuery!

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
-	$("header_img").attr("src",profile.getImageUrl());
-	$("header_name").text(profile.getName());
-	console.log("Test")
+	$("#header_img").attr("src",profile.getImageUrl());
+	$("#header_name").text(profile.getName());
+	console.log("Test");
 }
