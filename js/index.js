5330c6e5b9c81eb870a324e09d09747c74002371
Noger <noah@noger.ml>
2018-11-30 11:20:40 +0000

    0.2.2.1
    Fixed Typing Issue with Jquery

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	$("#header_img").attr("src",profile.getImageUrl());
 	$("#header_name").text(profile.getName());
-	$("#g-signin2").css("display","none");
+	$("#g-signinbutton").css("display","none");
 	$("#g-signout").css("display","block");
 	console.log("Test");
 }
@@ -11,7 +11,7 @@ function signOut() {
 	gapi.auth2.getAuthInstance().signOut();
 	$("#header_img").attr("src","https://i.stack.imgur.com/34AD2.jpg");
 	$("#header_name").text("Please Sign In");
-	$("#g-signin2").css("display","block");
+	$("#g-signinbutton").css("display","block");
 	$("#g-signout").css("display","none");
 	console.log("Signed Out");
 }
